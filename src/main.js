29c683af629519f95c4cca0986023e5f86e358b5
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 20:04:17 -0500

    feat(i18n): add internationalization support with english and spanish translations.

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import Material from '@primeuix/themes/material';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import {Avatar, Button, Card, Drawer, Image, Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
+import i18n from "./i18n.js";
 
 createApp(App)
+    .use(i18n)
     .use(PrimeVue, { ripple: true, theme: { preset: Material}})
     .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
