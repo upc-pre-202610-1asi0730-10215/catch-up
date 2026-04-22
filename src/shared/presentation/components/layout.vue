e34f9f1d58ed8d72449fd9191c766c4bf614e9c8
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 19:50:38 -0500

    feat(ui-layout): add layout component with menubar and toggle functionality.

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from "vue";
+
+const drawerVisible = ref(false);
+
+const toggleDrawer = () => {
+  drawerVisible.value = !drawerVisible.value;
+};
+
+</script>
+
+<template>
+<div>
+  <div>
+    <pv-menubar>
+      <template #start>
+        <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer"/>
+        <!-- Add Source list here -->
+      </template>
+      <template #end>
+        <!-- Add Language Switcher here -->
+      </template>
+    </pv-menubar>
+  </div>
+</div>
+  <div>
+    <!-- Add Article list here -->
+    <!-- Add Unavailable content alternate component here -->
+  </div>
+  <!-- Add Footer content here -->
+</template>
+
+<style scoped>
+
+</style>
