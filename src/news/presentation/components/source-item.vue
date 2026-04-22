e5ff499994d8a56297868a19172e164400321ed8
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 21:52:31 -0500

    feat(sources): add source-item component for displaying and selecting sources.

---
+++ src/news/presentation/components/source-item.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {Source} from "../../domain/model/source.entity.js";
+import {toRefs} from "vue";
+
+const props = defineProps({ source: { type: Source, required: true } });
+const emit = defineEmits(["source-selected"]);
+const { source } = toRefs(props);
+
+function emitSourceSelectedEvent() {
+  emit("source-selected", source);
+}
+</script>
+
+<template>
+<div class="m-4">
+  <div @click="emitSourceSelectedEvent" class="flex align-content-start flex-wrap">
+    <span class="flex align-items-center justify-content-center mr-2">
+      <pv-avatar :aria-label="source.name"
+                 :image="source.urlToLogo"
+                 shape="circle"/>
+    </span>
+    <span class="flex align-items-center justify-content-center">
+      {{ source.name }}
+    </span>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
