abdee053a887fe78e7b9a39e13de1bd4ad435920
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-23 20:08:57 -0500

    feat(layout): integrate source list and article list components with error handling.

--- src/shared/presentation/components/layout.vue
+++ src/shared/presentation/components/layout.vue
@@ -1,13 +1,33 @@
 <script setup>
-import {ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import FooterContent from "./footer-content.vue";
 import LanguageSwitcher from "./language-switcher.vue";
+import {newsStore} from "../../../news/application/news.store.js";
+import SourceList from "../../../news/presentation/components/source-list.vue";
+import ArticleList from "../../../news/presentation/components/article-list.vue";
+import UnavailableContent from "../../../news/presentation/components/unavailable-content.vue";
 
 const drawerVisible = ref(false);
 
 const toggleDrawer = () => {
   drawerVisible.value = !drawerVisible.value;
 };
+const sources = computed(() => newsStore.sources);
+const errors = computed(() => newsStore.errors);
+let articles = computed(() => newsStore.articles);
+const rerenderKey = ref(0);
+
+const setSource = source => {
+  newsStore.setCurrentSource(source);
+  articles = computed(() => newsStore.articles);
+  rerenderKey.value += 1; // Force re-render of the article list
+  toggleDrawer();
+}
+
+onMounted(() => {
+  newsStore.loadSources();
+  rerenderKey.value += 1;
+});
 
 </script>
 
@@ -17,7 +37,9 @@ const toggleDrawer = () => {
     <pv-menubar>
       <template #start>
         <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer"/>
-        <!-- Add Source list here -->
+        <source-list v-model:sources="sources"
+                     v-model:visible="drawerVisible"
+                     v-on:source-selected="setSource($event)"/>
       </template>
       <template #end>
         <language-switcher/>
@@ -26,8 +48,8 @@ const toggleDrawer = () => {
   </div>
 </div>
   <div>
-    <!-- Add Article list here -->
-    <!-- Add Unavailable content alternate component here -->
+    <article-list v-if="articles" v-model:articles="articles" :key="rerenderKey"/>
+    <unavailable-content v-else :errors="errors"/>
   </div>
   <footer-content/>
 </template>
