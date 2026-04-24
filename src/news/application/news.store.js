fdac75e7b05a088d760dba27422f4d3af10c083d
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-23 20:59:08 -0500

    feat(news): add logging for sources and articles response data in news store.

--- src/news/application/news.store.js
+++ src/news/application/news.store.js
@@ -20,6 +20,7 @@ export const newsStore = reactive({
     loadSources() {
         this.errors = [];
         newsApi.getSources().then(response => {
+            console.log(response);
             this.sources = SourceAssembler.toEntitiesFromResponse(response);
             if (this.sources.length > 0) {
                 this.setCurrentSource(this.sources[0]);
@@ -27,15 +28,19 @@ export const newsStore = reactive({
             }
         }).catch(error => {
             this.errors.push(error);
+            console.log(this.errors);
             this.sources = [];
         });
     },
     loadArticlesForCurrentSource() {
         if (this.currentSource === null) return;
         newsApi.getArticlesForSourceId(this.currentSource.id).then(response => {
+            console.log(response);
             this.articles = ArticleAssembler.withSource(this.currentSource).toEntitiesFromResponse(response);
+            console.log(this.articles);
         }).catch(error => {
             this.errors.push(error);
+            console.log(this.errors);
             this.articles = [];
         });
     }
