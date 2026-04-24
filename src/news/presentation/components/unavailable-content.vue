91b574966d70a136595249911c728aa3fc08b758
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-23 21:17:08 -0500

    fix(unavailable-content): correct spelling of 'localized' in documentation.

--- src/news/presentation/components/unavailable-content.vue
+++ src/news/presentation/components/unavailable-content.vue
@@ -4,7 +4,7 @@
  * @description Presentation component displayed when news content cannot be
  * loaded due to one or more API errors.
  *
- * Renders a localised unavailability message followed by the list of error
+ * Renders a localized unavailability message followed by the list of error
  * details collected by the application store. Typically shown in place of
  * {@link ArticleList} when the store's `errors` array is non-empty.
  *
@@ -15,6 +15,7 @@
  */
 import {useI18n} from "vue-i18n";
 import {toRefs} from "vue";
+import ArticleList from "./article-list.vue";
 
 const { t } = useI18n();
 const props = defineProps({ errors: null });
